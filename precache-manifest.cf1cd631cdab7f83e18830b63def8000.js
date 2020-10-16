b6fdadab12c64a7569f25c45d8e0a604fbb91c5b
Shari Reyes <shari2289@yahoo.com>
2020-10-16 15:26:39 -0500

    Updates

---
+++ precache-manifest.cf1cd631cdab7f83e18830b63def8000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d1ccd4d208d53b556711729bb2d90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d67093c689db2d28c2cb",
+    "url": "/static/css/main.908472bf.chunk.css"
+  },
+  {
+    "revision": "4edb2e7703b1c09f0055",
+    "url": "/static/js/2.7fa3983f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7fa3983f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d67093c689db2d28c2cb",
+    "url": "/static/js/main.fd5b1a70.chunk.js"
+  },
+  {
+    "revision": "e968eeb8c247134aad12",
+    "url": "/static/js/runtime-main.bdad0aff.js"
+  }
+]);
